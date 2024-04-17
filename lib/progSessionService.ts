ea0713a92ba511e38a68380d272b0daa519c81f6
Josh <JoshMontech@users.noreply.github.com>
2024-04-17 14:35:21 -0500

    updated prog session

--- lib/progSessionService.ts
+++ lib/progSessionService.ts
@@ -6,11 +6,11 @@ export async function getProgSessions(staticId: string) {
 }
 
 export async function getProgSession(staticId: string, progSessionId: string) {
-  const repsonse = await fetch(`${getAbsoluteUrl()}}/api/static/${staticId}/progsession/${progSessionId}`, {cache: 'no-store'})
+  const repsonse = await fetch(`${getAbsoluteUrl()}/api/static/${staticId}/progsession/${progSessionId}`, {cache: 'no-store'})
   return repsonse.json()
 }
 
 export async function getWipeFormProperties() {
-  const repsonse = await fetch(`${getAbsoluteUrl()}}/api/wipe-form-properties`)
+  const repsonse = await fetch(`${getAbsoluteUrl()}/api/wipe-form-properties`)
   return repsonse.json()
 }
