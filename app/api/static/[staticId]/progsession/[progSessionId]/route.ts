8764583a7766a5cbcd03eb4df9014a2d68c1df58
Josh <JoshMontech@users.noreply.github.com>
2024-04-16 19:05:25 -0500

    more GET adjustements?

--- app/api/static/[staticId]/progsession/[progSessionId]/route.ts
+++ app/api/static/[staticId]/progsession/[progSessionId]/route.ts
@@ -1,7 +1,7 @@
 import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
+export async function GET( request: NextRequest,
     { params }: { params: { staticId: string, progSessionId: string }}
 ) {
     const { progSessionId } = params
